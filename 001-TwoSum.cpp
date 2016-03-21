a67acf9c20173d92c82abb46462b426802032081
chenzhi87v5 <616407606@qq.com>
2016-03-21 22:16:12 +0800

    001-TwoSum.cpp

--- 001-TwoSum.cpp
+++ 001-TwoSum.cpp
@@ -11,10 +11,71 @@ return [0, 1].
 
  */
 
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+/*1-暴力解法:时间复杂度n2，超时*/
+/*
 class Solution {
 public:
-	vertor<int> twoSum(vector<int> num, int target) {
+	vector<int> twoSum(vector<int>& nums, int target) {
+		int in1 = 0, in2, temp;
+		vector<int> res;
 		
+		for (vector<int>::iterator it1 = nums.begin(); it1 != nums.end(); it1++){
+			temp = target - *it1;
+			in2 = in1 + 1;
+			for (vector<int>::iterator it2 = it1 + 1; it2 != nums.end(); it2++){
+				if (*it2 == temp){
+					res.push_back(in1);
+					res.push_back(in2);
+					return res;
+				}
+				in2++;	
+			}
+			in1++;
+		}
+		return res;
 	}
-
 };
+*/
+
+/*2-hash 哈希表存储每一个数对应的下标，时间复杂度n 空间复杂度n*/
+//unordered_map 使用、map使用、hash的理解 
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> mapping;
+		vector<int> result;
+		for (int i = 0; i < nums.size(); i++) {
+			mapping[nums[i]] = i;
+		}
+		for (int i = 0; i < nums.size(); i++) {
+			const int gap = target - nums[i];
+			if (mapping.find(gap) != mapping.end() && mapping[gap] > i) {
+				result.push_back(i);
+				result.push_back(mapping[gap]);
+				break;
+			}
+		}
+		return result;
+	}
+};
+
+
+int main()
+{
+	int nums[4] = {2, 7, 11, 15};
+	vector<int> num(nums, nums+4);
+	int target = 9;
+	Solution test;
+	
+	vector<int> res = test.twoSum(num, target);
+	for (vector<int>::iterator it = res.begin(); it != res.end(); it++){
+		cout<< *it <<endl;
+	}
+	return 0;
+}
