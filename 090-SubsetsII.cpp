8183ad2d8088467fe4cf427f5e5b2b3fd8d88e58
chenzhi87v5 <616407606@qq.com>
2016-04-18 22:32:04 +0800

    2016-4-18-Evening

---
+++ 090-SubsetsII.cpp
@@ -0,0 +1,48 @@
+/*
+Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+
+Note:
+	Elements in a subset must be in non-descending order.
+	The solution set must not contain duplicate subsets.
+For example,
+If nums = [1,2,2], a solution is:
+
+[
+ [2],
+ [1],
+ [1,2,2],
+ [2,2],
+ [1,2],
+ []
+]
+
+*/
+
+//1-:对比Subsets 题无重复元素相当于每个元素只能选0或者1次，这里扩充到每个元素可以选0到若干次
+//	增量构造法
+
+class Solution {
+public:
+	vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+
+		vector<vector<int>> result;
+		vector<int> path;
+
+		dfs(nums, nums.begin(), path, result);
+
+		return result;
+	}
+private:
+	static void dfs(const vector<int> &nums, vector<int>::iterator start, vector<int> &path, vector<vector<int>>&result) {
+		result.push_back(path); //加入path
+
+		for(auto i = start; i < nums.end(); i++) {
+			if (i != start && *i == *(i - 1))  //重复元素滤过
+				continue;
+			path.push_back(*i);
+			dfs(nums, i + 1, path,result);
+			path.pop_back();
+		}
+	}
+};
---
+++ 091-DecodeWays.cpp
@@ -0,0 +1,48 @@
+/*
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+
+Given an encoded message containing digits, determine the total number of ways to decode it.
+
+For example,
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+The number of ways decoding "12" is 2.
+
+*/
+
+//1-:动态规划 类似Climbing Stairs
+// f(n) = f(n - 1) + f(n - 2)
+//nums[i] 表示字符串s[0]到s[i - 1]解码方法
+class Solution {
+public:
+	int numDecodings(string s) {
+		if(s.empty())
+			return 0;
+		int *nums = new int[s.size() + 1];
+		int res;
+		
+		fill(nums, nums + s.size() + 1, 0)
+		nums[0] = 1;
+		nums[1] = s[0] != '0' ? 1 : 0;	  //只有一个数字并且不为0时有1个解法
+
+		for (int i = 2; i <= s.size(); i++) {
+			if (s[i - 1] != '0') {
+				nums[i] = nums[i - 1];
+			}
+
+			int twoDigits = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+			if (twoDigits >= 10 && twoDigits <= 26) {
+				nums[i] += nums[i - 2];
+			}
+		}
+		res = nums[s.size()];
+		delete nums;
+
+		return res;
+	}
+};
