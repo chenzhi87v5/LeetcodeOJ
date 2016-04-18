e7cb497bb10dd0da7eff9edfaf4c824ebea02ec4
chenzhi87v5 <616407606@qq.com>
2016-04-18 23:34:16 +0800

    2016-4-18-Evening

---
+++ 092-ReverseLinkedListII.cpp
@@ -0,0 +1,48 @@
+/*
+Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list.
+
+*/
+
+/**
+  * Definition for singly-linked list.
+  * struct ListNode {
+  *     int val;
+  *     ListNode *next;
+  *     ListNode(int x) : val(x), next(NULL) {}
+  * };
+  */
+
+//1-:比较复杂,使用头插法
+
+class Solution {
+public:
+	ListNode* reverseBetween(ListNode* head, int m, int n) {
+		ListNode dummy(-1);
+		dummy.next = head;
+
+		ListNode *prev = &dummy;
+		for (int i = 0 ; i < m - 1; i++)
+			prev = prev->next;
+		ListNode* const head2 = prev;
+		
+		prev = head2->next;
+		ListNode *cur = prev->next;
+		for (int i = m; i < n; i++) {  		//关键部分 循环头插法将prev后cur节点插入head2后面
+			prev->next = cur->next;
+			cur->next = head2->next;
+			head2->next = cur; 		//头插法
+			cur = prev->next;
+		}
+
+		return dummy.next;
+	}
+};
---
+++ 093-RestoreIPAddresses.cpp
@@ -0,0 +1,48 @@
+/*
+Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+For example:
+Given "25525511135",
+
+return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+
+*/
+
+//1-:深搜 + 递归
+
+class Solution {
+public:
+	vector<string> restoreIpAddresses(string s) {
+		vector<string> result;
+		string ip;  //中间结果
+		
+		dfs(s, 0, 0, ip, result);
+
+		return result;
+	}
+private:
+	void dfs(string s, size_t start, size_t step, string ip, vector<string> &result) {
+		if (start == s.size() && step == 4) {  //找到一个合法解
+			ip.resize(ip.size() - 1);       //删除最后一点'.'
+			result.push_back(ip);
+			return;
+		}
+
+		if (s.size() - start > (4 - step) * 3)
+			return; 		//剪枝，余下字符ip不能容下
+		if (s.size() - start < (4 - step))
+			return;  		//剪枝，余下字符不够ip书写
+		
+		int num = 0;
+		for (size_t i = start; i < start + 3; i++) {
+			num = num * 10 + (s[i] - '0');
+
+			if (num <= 255) {    	//当前节点合法，可以继续往下递归
+				ip += s[i];
+				dfs(s, i + 1, step + 1, ip + '.', result);
+			}
+			if (num == 0)  		//不允许前缀为0,但是允许单个0
+				break;
+		}
+	}
+};
