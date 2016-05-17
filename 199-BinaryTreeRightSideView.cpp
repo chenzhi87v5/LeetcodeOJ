13df0338c043f1c6bbe4ffa7cc44e292b2c7adfd
chenzhi87v5 <616407606@qq.com>
2016-05-17 11:17:17 +0800

    2016-5-15

---
+++ 199-BinaryTreeRightSideView.cpp
@@ -0,0 +1,54 @@
+/*
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+  
+  You should return [1, 3, 4].
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+//层次遍历每一层最后一个节点，由上往下
+//复杂度 logn 空间复杂度 1 ???
+class Solution {
+public:
+	vector<int> rightSideView(TreeNode* root) {
+		vector<int> result;
+		if (root == NULL)
+			return result;
+		queue<TreeNode *> que;   //层次访问，使用队列
+		que.push(root);
+		TreeNode *curr;         //当前节点
+
+		while(!que.empty()) {
+			int cnt = que.size();
+			for (int i = 0; i < cnt; i++) {
+				curr = que.front();
+				que.pop();
+				if (curr->left) {
+					que.push(curr->left);
+				}
+				if (curr->right) {
+					que.push(curr->right);
+				}
+			}
+			result.push_back(curr->val);
+		}
+		
+		return result;
+	}
+};
