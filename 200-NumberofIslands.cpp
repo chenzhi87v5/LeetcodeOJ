60a7377f56d2d189d4e3a3f252a111014b3ac4a0
chenzhi87v5 <616407606@qq.com>
2016-05-17 14:44:03 +0800

    2016-5-16

---
+++ 200-NumberofIslands.cpp
@@ -0,0 +1,59 @@
+/*
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+11110
+11010
+11000
+00000
+Answer: 1
+
+Example 2:
+
+11000
+11000
+00100
+00011
+Answer: 3
+*/
+
+//实为求连通子图的个数，借助DFS的思想
+//DFS 递归法
+class Solution {
+public:
+	int numIslands(vector<vector<char>>& grid) {
+		int num = 0;
+		//参数检查
+		if (grid.size() == 0) {
+			return num;
+		}
+		if (grid[0].size() == 0) {
+			return num;
+		}
+		
+		for (int i = 0; i < grid.size(); i++) {
+			for (int j = 0; j < grid[i].size(); j++) {
+				if (dfs(i, j, grid))
+					num++;
+			}
+		}
+
+		return num;
+	}
+
+private:
+	//将所有相邻的1置为3
+	bool dfs(int i, int j, vector<vector<char>> &grid) {
+		if (i >= 0 && i < grid.size() && j >=0 && j <grid[i].size() && grid[i][j] == '1') {
+			grid[i][j] = '3';
+			dfs(i - 1, j, grid);
+			dfs(i + 1, j, grid);
+			dfs(i, j - 1, grid);
+			dfs(i, j + 1, grid);
+			return true;
+		}
+
+		return false;
+	}
+};
