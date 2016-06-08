ebf448c340183d5543309f409131a2e89e69aed2
chenzhi87v5 <616407606@qq.com>
2016-06-08 15:41:00 +0800

    2016-6-7

---
+++ 306-AdditiveNumber.cpp
@@ -0,0 +1,59 @@
+/*
+Additive number is a string whose digits can form additive sequence.
+
+A valid additive sequence should contain at least three numbers. Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
+
+For example:
+"112358" is an additive number because the digits can form an additive sequence: 1, 1, 2, 3, 5, 8.
+
+1 + 1 = 2, 1 + 2 = 3, 2 + 3 = 5, 3 + 5 = 8
+
+"199100199" is also an additive number, the additive sequence is: 1, 99, 100, 199.
+
+1 + 99 = 100, 99 + 100 = 199
+
+Note: Numbers in the additive sequence cannot have leading zeros, so sequence 1, 2, 03 or 1, 02, 3 is invalid.
+
+Given a string containing only digits '0'-'9', write a function to determine if it's an additive number.
+
+Follow up:
+How would you handle overflow for very large input integers?
+*/
+
+//前两个数和生成下一个数
+/*
+思路:
+这道题定义了一种加法数，就是至少含有三个数字，除去前两个数外，每个数字都是前面两个数字的和，题目中给了许多例子，也限定了一些不合法的情况，比如两位数以上不能以0开头等等，让我们来判断一个数是否是加法数。这题可用Brute Force的思想来解，我们让第一个数字先从一位开始，第二个数字从一位，两位，往高位开始搜索，前两个数字确定了，相加得到第三位数字，三个数组排列起来形成一个字符串，和原字符串长度相比，如果小于原长度，那么取出上一次计算的第二个和第三个数，当做新一次计算的前两个数，用相同的方法得到第三个数，再加入当前字符串，再和原字符串长度相比，以此类推，直到当前字符串长度不小于原字符串长度，比较两者是否相同，相同返回true，不相同则继续循环。如果所有情况都遍历完了还是没有返回true，则说明不是Additive Number，返回false，参见代码如下：
+*/
+
+//代码非常值得学习
+class Solution {
+public:
+	bool isAdditiveNumber(string num) {
+		for (int i = 1; i < num.size(); i++) {
+			for (int j = i + 1; j < num.size(); j++) {
+				string s1 = num.substr(0, i);                   //string.substr(i, n);
+				string s2 = num.substr(i, j - i);
+				long long d1 = atoll(s1.c_str()), d2 = atoll(s2.c_str());         //string 转 long long 、string.c_str()、atoll、atoi
+				
+				if ((s1.size() > 1 && s1[0] == '0') || (s2.size() > 1 && s2[0] == '0'))           //不合法情况
+					continue;
+				
+				long long next = d1 + d2;
+				string nexts = to_string(next);                      //long long 转 string  to_string(数字)
+				string now = s1 + s2 + nexts;
+
+				while (now.size() < num.size()) {
+					d1 = d2;
+					d2 = next;
+					next = d1 + d2;
+					nexts = to_string(next);
+					now += nexts;
+				}
+				if (now == num)
+					return true;
+			}
+		}
+		return false;
+	}
+};
---
+++ 307-RangeSumQuery-Mutable.cpp
@@ -0,0 +1,75 @@
+/*
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+The update(i, val) function modifies nums by updating the element at index i to val.
+Example:
+ Given nums = [1, 3, 5]
+
+ sumRange(0, 2) -> 9
+ update(1, 2)
+ sumRange(0, 2) -> 8
+
+Note:
+	The array is only modifiable by the update function.
+	You may assume the number of calls to update and sumRange function is distributed evenly.
+*/
+
+//Range Sum Query - Immutable 区域和检索 - 不可变的延伸
+
+/*
+原码，补码和反码。计算机中，数值都是以补码的形式保存
+补码：正数的补码和原码一样;负数的补码，就是在原来的原码基础上，保持符号位不变，其他位取反，然后再加1
+
+最低位的计算方法有两种，一种是x&(x^(x–1))，另一种是利用补码特性x&-x 
+*/
+
+/*
+这道题我们要使用一种新的数据结构，叫做树状数组Binary Indexed Tree，这是一种查询和修改复杂度均为O(logn)的数据结构。这个树状数组比较有意思，所有的奇数位置的数字和原数组对应位置的相同，偶数位置是原数组若干位置之和 
+*/
+
+//详细参考 http://www.cnblogs.com/grandyang/p/4985506.html
+
+class NumArray {
+private:
+	vector<int> num;  //存储 原数组
+	vector<int> bit;  //存储 树状数组Binary Indexed Tree
+
+public:
+	NumArray(vector<int> &nums) {
+		num.resize(nums.size() + 1);
+		bit.resize(nums.size() + 1);
+		
+		for (int i = 0; i < nums.size(); i++) {
+			update(i, nums[i]);
+		}
+	}
+
+	void update(int i, int val) {
+		int diff = val - num[i + 1];
+		
+		for (int j = i + 1; j < num.size(); j += (j & -j)) {
+			bit[j] += diff;
+		}
+		
+		num[i + 1] = val;
+	}
+
+	int sumRange(int i, int j) {
+		return getSum(j + 1) - getSum(i);
+	}
+	
+	int getSum(int i) {
+		int res = 0;
+		for (int j = i; j > 0; j -= (j & -j)) {
+			res += bit[j];
+		}
+		return res;
+	}
+};
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.update(1, 10);
+// numArray.sumRange(1, 2);
