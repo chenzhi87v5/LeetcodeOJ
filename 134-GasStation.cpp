6ee59fc03c116e6cc6a0bc8dca2fee81e3bd1f01
chenzhi87v5 <616407606@qq.com>
2016-07-21 20:40:33 +0800

    Greedy_7-21

--- 134-GasStation.cpp
+++ 134-GasStation.cpp
@@ -1,34 +1,29 @@
+//Gas Station 加油站问题
 /*
 There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
-
 You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
-
 Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
 
 Note:
 	The solution is guaranteed to be unique.
 */
-
-//1-:mySolution 首先想到的是n^2 的解法，对每个点进行模拟
-//2-:时间复杂度为n的解法 空间复杂度 1
-	//设置两个变量，sum判断当前有效性 total判断整个数组是否有解，有就返回通过sum 得到的下标，没有则返回-1。
-
+/*
+这道转圈加油问题不算很难，只要想通其中的原理就很简单。我们首先要知道能走完整个环的前提是gas的总量要大于cost的总量，这样才会有起点的存在。假设开始设置起点start = 0, 并从这里出发，如果当前的gas值大于cost值，就可以继续前进，此时到下一个站点，剩余的gas加上当前的gas再减去cost，看是否大于0，若大于0，则继续前进。当到达某一站点时，若这个值小于0了，则说明从起点到这个点中间的任何一个点都不能作为起点，则把起点设为下一个点，继续遍历。当遍历完整个环时，当前保存的起点即为所求。代码如下： 
+*/
 class Solution {
 public:
 	int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-		int total = 0;
-		int j = -1;
-		for (int i = 0, sum = 0; i < gas.size(); i++) {
+		int total = 0, sum = 0, start = 0;
+		for (int i = 0; i < gas.size(); i++) {
 			sum += gas[i] - cost[i];
 			total += gas[i] - cost[i];
 			//判断当前有效性
 			if (sum < 0) {
-				j = i;
+				start = i + 1;
 				sum = 0;
 			}
 		}
-
 		//判断全局有效性
-		return total >= 0 ? j + 1: -1;
+		return total >= 0 ? start : -1;
 	}
 };
