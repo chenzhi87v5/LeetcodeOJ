243e94bdb0e48add2e313cef4f23dee656ec70be
chenzhi87v5 <616407606@qq.com>
2016-08-18 17:34:50 +0800

    JinDong

--- Jindong_TreeCopy.cpp
+++ Jindong_TreeCopy.cpp
@@ -8,15 +8,26 @@ struct Node{
 实现这样的“树”的拷贝 使用map结构
 */
 
-Node* cloneTree(Node* root, Node *copy) {
+Node* cloneTree(Node* root, Node *copy, map<Node*, Node*> &cmap, map<Node*, Node*> &nmap) {
 	if (!root)
 		return NULL;
 	copy = new Node(root->value);
+	
 	
 	if (root->left)
 		copy->left = clone(root->left, copy->left);
 	if (root->right)
 		coyt->right = clone(root->right, copy->right);
-	//加上 map 复制叶节点
+	
+	nmap[root] = copy;
+	if (root->leaf)
+		cmap[root] = copy;
 	return copy;
+}
+
+int cloneLeaf(map<Node*, Node*> &cmap, map<Node*, Node*> &nmap) {
+	for (auto a : cmap) {
+		a.second->leaf = nmap[a.first->leaf];
+	}
+	return 0;
 }
